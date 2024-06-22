b08d78d41ae768b0903a2956ec67671ccfd03d6b
Ali Moghanni <a.moghanni@gmail.com>
2024-06-22 18:19:11 +0200

    Initial commit

---
+++ 00_Introduction/src/HelloWorld.java
@@ -0,0 +1,12 @@
+// 00_Introduction/HelloWorld.java
+/*
+ * Introduction to Java
+ * This program prints "Hello, World!" to the console.
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print Hello World to the console
+        System.out.println("Hello, World!");
+    }
+}
